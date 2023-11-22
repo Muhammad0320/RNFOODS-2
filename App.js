97fa0c3a4dec489712ff7569bd637ca8bd496fb0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-22 10:45:15 +0100

    chore: add navigation to app.js

--- App.js
+++ App.js
@@ -2,15 +2,18 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import MealCategories from "./screens/MealCategories";
 
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  return <MealCategories />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator> </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
