45b212dde4a1ddfab12450a700630ca28601261b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-22 11:27:38 +0100

    chore: send data with the id params

--- screens/MealCategories.jsx
+++ screens/MealCategories.jsx
@@ -2,11 +2,12 @@ import { FlatList } from "react-native";
 import CategoryGridTile from "../components/CategoryGridTile";
 import { CATEGORIES } from "../data/test-data";
 
-const testCat = [1, 2, 3, 5, , 6, 7, 8, 9, 10];
-
 function MealCategories({ navigation }) {
-  const handlePress = () => {
-    navigation.navigate("MealOverview");
+  const handlePress = (id) => {
+    navigation.navigate("MealOverview"),
+      {
+        categoryId: id,
+      };
   };
 
   return (
