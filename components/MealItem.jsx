682c193cea2545b4a2241e578a17045a109a7a48
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-22 12:36:59 +0100

    chore: create all the styles

--- components/MealItem.jsx
+++ components/MealItem.jsx
@@ -1,4 +1,11 @@
-import { Image, Pressable, Text, View } from "react-native";
+import {
+  Image,
+  Platform,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 function MealItem({ title, affordability, complexity, imageUrl, duration }) {
   return (
@@ -19,3 +26,38 @@ function MealItem({ title, affordability, complexity, imageUrl, duration }) {
 }
 
 export default MealItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 16,
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOffset: { height: 0, width: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    margin: 16,
+  },
+
+  image: {
+    width: "100%",
+    height: 200,
+    objectFit: "cover",
+  },
+
+  title: {
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "600",
+  },
+
+  detailContainer: {
+    flexDirection: "row",
+    columnGap: 6,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
