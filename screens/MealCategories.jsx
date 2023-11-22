7c56f7d762a9993376a8dc3f2bf8d8591f2ddc66
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-22 11:13:02 +0100

    chore: add login to switch betwoon different screens

--- screens/MealCategories.jsx
+++ screens/MealCategories.jsx
@@ -4,14 +4,18 @@ import { CATEGORIES } from "../data/test-data";
 
 const testCat = [1, 2, 3, 5, , 6, 7, 8, 9, 10];
 
-function MealCategories() {
+function MealCategories({ navigation }) {
+  const handlePress = () => {
+    navigation.navigate("MealOverview");
+  };
+
   return (
     <FlatList
       data={CATEGORIES}
       numColumns={2}
       keyExtractor={(item) => item.id}
       renderItem={({ item: { color, title } }) => (
-        <CategoryGridTile title={title} color={color} />
+        <CategoryGridTile title={title} color={color} onPress={handlePress} />
       )}
     />
   );
