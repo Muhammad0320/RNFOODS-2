db92d5775d7119f71df7afd15d8a9b9df4fcad5a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-22 12:25:26 +0100

    chore: create structure for the meal item comp

--- components/MealItem.jsx
+++ components/MealItem.jsx
@@ -5,6 +5,14 @@ function MealItem({ title, affordability, complexity, imageUrl, duration }) {
     <View>
       <Pressable>
         <Image source={{ uri: imageUrl }} />
+        <View>
+          <Text> {title} </Text>
+          <View>
+            <Text> {duration}m </Text>
+            <Text> {complexity.toUpperCase()} </Text>
+            <Text> {affordability.toUpperCase()} </Text>
+          </View>
+        </View>
       </Pressable>
     </View>
   );
