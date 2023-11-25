29aeec9a97d88868b9529c825a16de8325f97801
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-25 14:55:28 +0100

    chore: complete meal info page

--- components/MealsInfo.jsx
+++ components/MealsInfo.jsx
@@ -1,5 +1,28 @@
-function MealsInfo() {
-  return <View></View>;
+import { StyleSheet } from "react-native";
+
+function MealsInfo({ duration, complexity, affordability }) {
+  return (
+    <View style={styles.detailContainer}>
+      <Text> {duration}m </Text>
+      <Text> {complexity.toUpperCase()} </Text>
+      <Text> {affordability.toUpperCase()} </Text>
+    </View>
+  );
 }
 
 export default MealsInfo;
+
+const styles = StyleSheet.create({
+  textContainer: {
+    marginVertical: 15,
+    rowGap: 8,
+    marginHorizontal: 20,
+  },
+
+  detailContainer: {
+    flexDirection: "row",
+    columnGap: 6,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
