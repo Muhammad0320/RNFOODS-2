68addcd497fe30ada74e302ac0ba0f2ab62e2336
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-25 15:56:25 +0100

    chore: update list comp

--- components/details/List.jsx
+++ components/details/List.jsx
@@ -1,5 +1,30 @@
+import { StyleSheet } from "react-native";
+
 function List({ data }) {
-  return <View></View>;
+  return data.map((dataPoint) => (
+    <View style={styles.container}>
+      <Text style={styles.text} key={dataPoint}>
+        {" "}
+        {dataPoint}{" "}
+      </Text>
+    </View>
+  ));
 }
 
 export default List;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    marginHorizontal: 12,
+    marginVertical: 6,
+  },
+
+  text: {
+    backgroundColor: "#e3b4a7",
+    color: "#351401",
+    fontSize: 15,
+    textAlign: "center",
+  },
+});
