5500d9c3c367659d99bfbd627492c4c3f78b0e97
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-25 15:04:06 +0100

    chore: compute mealsDetails data

--- screens/MealsDetails.jsx
+++ screens/MealsDetails.jsx
@@ -1,9 +1,48 @@
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
+import { MEALS } from "../data/test-data";
+import MealsInfo from "../components/MealsInfo";
 
 function MealsDetails({ route }) {
   const { id } = route.params;
 
-  return <Text>This is meal details page - {id}</Text>;
+  const {
+    affordability,
+    complexity,
+    duration,
+    ingredients,
+    steps,
+    title,
+    imageUrl,
+  } = MEALS.find((meals) => meals.id === id);
+
+  return (
+    <View>
+      <Image source={{ uri: imageUrl }} />
+      <Text> {title} </Text>
+
+      <MealsInfo
+        affordability={affordability}
+        complexity={complexity}
+        duration={duration}
+      />
+
+      <Text> Ingredient </Text>
+
+      <View>
+        {ingredients.map((ingredient) => (
+          <Text key={ingredient}> {ingredient} </Text>
+        ))}
+      </View>
+
+      <Text> Steps </Text>
+
+      <View>
+        {steps.map((ingredient) => (
+          <Text key={ingredient}> {ingredient} </Text>
+        ))}
+      </View>
+    </View>
+  );
 }
 
 export default MealsDetails;
