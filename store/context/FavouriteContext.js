a242b0b17ed5e9307c2893fa00775eafe489b72e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-28 05:13:28 +0100

    chore: complete fav contenxt comp

--- store/context/FavouriteContext.js
+++ store/context/FavouriteContext.js
@@ -1,3 +1,39 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
-const FavouriteContext = createContext();
+const FavouriteContext = createContext({
+  ids: [],
+  addFavorite: (id) => {},
+  removeFavorite: (id) => {},
+});
+
+export const FavouriteContextProvider = ({ children }) => {
+  const [favIds, setFavId] = useState([]);
+
+  const addFavorite = (id) => {
+    setFavId((currentFav) => [...currentFav, id]);
+  };
+
+  const removeFavorite = (id) => {
+    setFavId((currentFav) =>
+      currentFav.filter((currentId) => currentId !== id)
+    );
+  };
+
+  return (
+    <FavouriteContext.Provider
+      value={{ ids: favIds, addFavorite, removeFavorite }}
+    >
+      {" "}
+      {children}{" "}
+    </FavouriteContext.Provider>
+  );
+};
+
+export const useFavouriteContext = () => {
+  const context = useContext(FavouriteContext);
+
+  if (!context)
+    throw new Error(" Favourite context was used outside its context provider");
+
+  return context;
+};
