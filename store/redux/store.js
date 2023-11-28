8085f2ef979a23ff297eba57df1c16e4c68a700c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-28 07:20:22 +0100

    chore : add reducer to to store

--- store/redux/store.js
+++ store/redux/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import favouriteSlice from "./favouriteSlice";
+
 export const store = configureStore({
-  reducer: {},
+  reducer: { favourite: favouriteSlice },
 });
