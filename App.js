5fe28280f525d29981d60e27c94aa5ee57676732
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-28 05:15:10 +0100

    chore: wrapped context provider aroud root file

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import MealCategories from "./screens/MealCategories";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { FavouriteContextProvider } from "./store/context/FavouriteContext";
 
 const Stack = createNativeStackNavigator();
 
@@ -35,26 +36,28 @@ const DrawerNavigator = () => {
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: { backgroundColor: "#351401" },
-          contentStyle: { backgroundColor: "#3f2f25" },
-          headerTintColor: "#fff",
-        }}
-      >
-        <Stack.Screen
-          name="MealCategories"
-          component={DrawerNavigator}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="MealDetails"
-          component={MealsDetails}
-          options={{ title: "About the meal" }}
-        />
-        <Stack.Screen name="MealOverview" component={MealsOverView} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <FavouriteContextProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#351401" },
+            contentStyle: { backgroundColor: "#3f2f25" },
+            headerTintColor: "#fff",
+          }}
+        >
+          <Stack.Screen
+            name="MealCategories"
+            component={DrawerNavigator}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="MealDetails"
+            component={MealsDetails}
+            options={{ title: "About the meal" }}
+          />
+          <Stack.Screen name="MealOverview" component={MealsOverView} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </FavouriteContextProvider>
   );
 }
